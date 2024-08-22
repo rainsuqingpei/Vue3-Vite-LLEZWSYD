ee413c0174db135d63eed4d3ceac36b757c40a2f
rainsuqingpei <454231850@qq.com>
2024-08-22 17:59:00 +0800

    nothing

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,7 +1,28 @@
 <template>
-  <el-button type="primary">按钮</el-button>
+  <div>
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-link to="/formalHomes">Go to formalHomes</router-link>
+    <!-- 路由匹配到的组件将渲染在这里 -->
+
+    <div>About.vue</div>
+    <div>width: {{ width }}</div>
+    <div>height: {{ height }}</div>
+    <div>currentTime: {{ currentTime }}</div>
+    <div>Mouse position is at: {{ mouse.x }}, {{ mouse.y }}</div>
+  </div>
 </template>
 <script setup>
-import { ref, reactive, onMounted, onUnmounted, inject } from "vue";
+import { ref, reactive, onMounted, onUnmounted, getCurrentInstance } from "vue";
+import { useWindowSize } from "../components/useWindowSize";
+import { useNowTime } from "../components/useNowTime";
+import { useMousePosition } from "../components/useMousePosition.js";
+
+const { width, height } = useWindowSize();
+const { currentTime } = useNowTime();
+const mouse = useMousePosition();
+
+//获取当前组件的实例、上下文来操作router和vuex等。相当于this
+const instance = getCurrentInstance();
+console.log(instance.proxy);
 </script>
 <style scoped></style>
